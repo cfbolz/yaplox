7daa32475bc6fbf9d9ac6c713f5495acbde04ac5
Carl Friedrich Bolz-Tereick <cfbolz@gmx.de>
2021-01-19 20:28:22 +0100

    bad argument handling of --jit

--- target.py
+++ target.py
@@ -1,10 +1,24 @@
 import sys
+
+from rpython.rlib import jit
+
 from yaplox.main import Yaplox
 
 def target(*args):
     return main
 
 def main(argv):
+    # terrible argument handling
+    for i in range(len(argv)):
+        if argv[i] == "--jit":
+            if len(argv) == i + 1:
+                print "missing argument after --jit"
+                return 2
+            jitarg = argv[i + 1]
+            del argv[i:i+2]
+            jit.set_user_param(None, jitarg)
+            break
+
     if len(argv) != 2:
         print("Usage: %s [script]" % (argv[0], ))
         return 64
